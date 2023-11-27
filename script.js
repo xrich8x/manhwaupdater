53383fe722f91cccf587d5e8663dd988a3befff9
xrich8x <152074473+xrich8x@users.noreply.github.com>
2023-11-27 13:46:11 +0800

    java

---
+++ script.js
@@ -0,0 +1,17 @@
+// script.js
+
+function addData() {
+    // Get the table reference
+    var table = document.getElementById("mangaTable");
+
+    // Create a new row
+    var newRow = table.insertRow(table.rows.length);
+
+    // Sample data, you can replace this with user input or fetch from an API
+    var data = ["New Data", "New Website", "New Update", "New Opened"];
+
+    // Loop through the data and add cells to the new row
+    for (var i = 0; i < data.length; i++) {
+        var cell = newRow.insertCell(i);
+        cell.innerHTML = data[i];
+    }
